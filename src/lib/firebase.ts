2a340b0dcd04c055199a84fe0ae494020a582439
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-07-27 11:08:49 +0000

    again same error. why am i getting the error ?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -18,18 +18,20 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 // Enable offline persistence
-enableIndexedDbPersistence(db)
-  .catch((err) => {
-    if (err.code == 'failed-precondition') {
-      // Multiple tabs open, persistence can only be enabled
-      // in one tab at a time.
-      console.warn('Firestore persistence failed: failed-precondition. Multiple tabs open?');
-    } else if (err.code == 'unimplemented') {
-      // The current browser does not support all of the
-      // features required to enable persistence
-       console.warn('Firestore persistence failed: unimplemented. Browser not supported.');
-    }
-  });
+if (typeof window !== 'undefined') {
+  enableIndexedDbPersistence(db)
+    .catch((err) => {
+      if (err.code == 'failed-precondition') {
+        // Multiple tabs open, persistence can only be enabled
+        // in one tab at a time.
+        console.warn('Firestore persistence failed: failed-precondition. Multiple tabs open?');
+      } else if (err.code == 'unimplemented') {
+        // The current browser does not support all of the
+        // features required to enable persistence
+         console.warn('Firestore persistence failed: unimplemented. Browser not supported.');
+      }
+    });
+}
 
 
 export { app, auth, db };
